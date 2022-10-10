56eb040a78fd8d00dd5cbcb63a107807616a1a36
Paulo Santos <paulohfsantos1@gmail.com>
2022-10-10 01:40:57 -0300

    add cors

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 import { config } from "dotenv";
 import express, { Application } from "express";
 import routes from "./routes";
+import cors from "cors";
 
 config();
 
 const app: Application = express();
 
-app.use(express.json())
-app.use(routes)
+app.use(cors());
+app.use(express.json());
+app.use(routes);
 app.use(express.urlencoded({ extended: true }));
 
-const port = process.env.API_PORT
+const port = process.env.API_PORT;
 
-app.listen(port)
+app.listen(port);
